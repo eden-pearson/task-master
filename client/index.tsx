6dcfd2d73b34d500bb95d3052e8e23c0cf6c5eb6
Eden Pearson <pearsoneden@gmail.com>
2024-05-15 10:47:37 +1200

    fix auth issues

--- client/index.tsx
+++ client/index.tsx
@@ -2,20 +2,24 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Auth0Provider } from '@auth0/auth0-react'
-
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { routes } from './routes'
 
 const queryClient = new QueryClient()
 export const router = createBrowserRouter(routes)
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN!
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!
+const redirectUri = process.env.REACT_APP_AUTH0_REDIRECT_URI!
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE!
+
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
-      domain="mako-2023-eden.au.auth0.com"
-      clientId="eczwHXmWpQ6y9wF75nDHXrECrr0FA6wu"
-      redirectUri={window.location.origin}
-      audience="https://taskmaster/api"
+      domain={domain}
+      clientId={clientId}
+      redirectUri={redirectUri}
+      audience={audience}
     >
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
